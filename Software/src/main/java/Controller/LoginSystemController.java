f8cfa6dded76beef29264aa28a960595b0401fe5
pti4life <kallai.roland@outlook.com>
2019-09-26 20:06:24 +0200

    Bejelentkező/Regisztrációs rendszer back-end vázának megkonstruálása

--- Software/src/main/java/Controller/LoginSystemController.java
+++ Software/src/main/java/Controller/LoginSystemController.java
@@ -1,17 +1,59 @@
 package Controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginSystemController implements Initializable {
 
+    //SignUp
+    @FXML
+    TextField regName;
+
+    @FXML
+    TextField regUserame;
+
+    @FXML
+    PasswordField regPassword1;
+
+    @FXML
+    PasswordField regPassword2;
+
+
+    //Login
+    @FXML
+    TextField loginUsername;
+
+    @FXML
+    PasswordField loginPassword;
+
+
 
 
     @FXML
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
+    }
+
+
+    public void backToLoginScreen(ActionEvent actionEvent) {
+
+    }
+
+    public void SignUp(ActionEvent actionEvent) {
+
+    }
+
+    public void Login(ActionEvent actionEvent) {
+    }
+
+    public void GoToRegScreen(ActionEvent actionEvent) {
 
     }
 }
---
+++ Software/src/main/java/Modell/LoginSystemModell.java
@@ -0,0 +1,21 @@
+package Modell;
+
+public class LoginSystemModell {
+
+
+    public String login() {
+
+        return null;
+    }
+
+    public String signUp() {
+
+        return null;
+    }
+
+    private boolean authUser() {
+
+        return true;
+    }
+
+}
