c8a01603172c10fc6ee1d49a96f706d7da6a690a
pti4life <kallai.roland@outlook.com>
2019-09-29 21:27:08 +0200

    Jelszó hashelés implementálása

--- Software/src/main/java/Modell/LoginSystemModell.java
+++ Software/src/main/java/Modell/LoginSystemModell.java
@@ -1,7 +1,6 @@
 package Modell;
 
-import javax.xml.crypto.Data;
-import java.util.IllegalFormatCodePointException;
+import org.apache.commons.codec.digest.DigestUtils;
 
 public class LoginSystemModell {
 
@@ -11,6 +10,7 @@ public class LoginSystemModell {
         String res=db.authUser(username);
         System.out.println(res);
         if (res.equals("USER_EXISTS")) {
+            password=DigestUtils.sha256Hex(password);
             String loginResult=db.login(username,password);
             return loginResult;
         }
@@ -19,6 +19,7 @@ public class LoginSystemModell {
     }
 
     public String signUp(String username, String password, String password2) {
+
         if (!password.equals(password2)) {
             return "TWO_PASSWORD_NOT_EQUAL";
         }
@@ -26,12 +27,13 @@ public class LoginSystemModell {
         if (password.equals(username)) {
             return "PASSWORD_EQUAL_USERNAME";
         }
-
+;
         String result=db.authUser(username);
-
-        if (result.equals("USER_EXISTS")) {
+        if (result.equals("USER_DOESNT_EXISTS")) {
+            password= DigestUtils.sha256Hex(password);
+            System.out.println("LEFUT");
             String result2=db.addUser(username,password);
-            return result;
+            return result2;
         } else  {
             return result;
         }
